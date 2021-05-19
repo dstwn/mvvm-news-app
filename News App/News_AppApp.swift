e22645e8054e844855932576896c161eebb5a405
Dwi Setiawan <dwisetiawan@linuxmail.org>
2021-05-19 12:52:59 +0700

    Initial Commit

---
+++ News App/News_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_AppApp.swift
+//  News App
+//
+//  Created by Dwi Setiawan on 19/05/21.
+//
+
+import SwiftUI
+
+@main
+struct News_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
